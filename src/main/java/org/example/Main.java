b101e75cfcae8b371b6d9ff9adf4607f7cb363f4
GiovaneGuimaraes <gioguima24@gmail.com>
2024-04-14 20:17:07 -0300

    feat: Make a reader for week temperator average

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.models.TemperaturaValores;
+
+public class Main {
+    public static void main(String[] args) {
+        //Exemplo 1
+        TemperaturaValores semana = new TemperaturaValores(2, 2, 2, 2, 2, 2, 3);
+        int[] valores = { semana.getTempSegunda(), semana.getTempTerca(), semana.getTempQuarta(), semana.getTempQuinta(), semana.getTempSexta(), semana.getTempSabado(), semana.getTempDomingo()};
+        System.out.println("A semana 1 possui " + TemperaturaValores.valoresAcimaMedia(valores) + " valor(es) acima da média");
+
+        //Exemplo 2
+        TemperaturaValores semana2 = new TemperaturaValores(21, 10, 13, 34, 30, 21, 34);
+        int[] valores1 = { semana2.getTempSegunda(), semana2.getTempTerca(), semana2.getTempQuarta(), semana2.getTempQuinta(), semana2.getTempSexta(), semana2.getTempSabado(), semana2.getTempDomingo()};
+        System.out.println("A semana 2 possui " + TemperaturaValores.valoresAcimaMedia(valores1) + " valor(es) acima da média");
+
+        TemperaturaValores semana3 = new TemperaturaValores(2, 2, 2, 2, 2, 2, 1);
+        int[] valores3 = { semana3.getTempSegunda(), semana3.getTempTerca(), semana3.getTempQuarta(), semana3.getTempQuinta(), semana3.getTempSexta(), semana3.getTempSabado(), semana3.getTempDomingo()};
+        System.out.println("A semana 3 possui " + TemperaturaValores.valoresAcimaMedia(valores3) + " valor(es) acima da média");
+    }
+}
---
+++ src/main/java/org/example/models/TemperaturaValores.java
@@ -0,0 +1,108 @@
+package org.example.models;
+
+public class TemperaturaValores {
+    public TemperaturaValores(int tempSegunda, int tempTerca, int tempQuarta, int tempQuinta, int tempSexta, int tempSabado, int tempDomingo) {
+        this.tempSegunda = tempSegunda;
+        this.tempTerca = tempTerca;
+        this.tempQuarta = tempQuarta;
+        this.tempQuinta = tempQuinta;
+        this.tempSexta = tempSexta;
+        this.tempSabado = tempSabado;
+        this.tempDomingo = tempDomingo;
+    }
+
+    private int tempSegunda;
+    private int tempTerca;
+    private int tempQuarta;
+    private int tempQuinta;
+    private int tempSexta;
+    private int tempSabado;
+    private int tempDomingo;
+
+    public int getTempSegunda() {
+        return tempSegunda;
+    }
+
+    public void setTempSegunda(int tempSegunda) {
+        this.tempSegunda = tempSegunda;
+    }
+
+    public int getTempTerca() {
+        return tempTerca;
+    }
+
+    public void setTempTerca(int tempTerca) {
+        this.tempTerca = tempTerca;
+    }
+
+    public int getTempQuarta() {
+        return tempQuarta;
+    }
+
+    public void setTempQuarta(int tempQuarta) {
+        this.tempQuarta = tempQuarta;
+    }
+
+    public int getTempQuinta() {
+        return tempQuinta;
+    }
+
+    public void setTempQuinta(int tempQuinta) {
+        this.tempQuinta = tempQuinta;
+    }
+
+    public int getTempSexta() {
+        return tempSexta;
+    }
+
+    public void setTempSexta(int tempSexta) {
+        this.tempSexta = tempSexta;
+    }
+
+    public int getTempSabado() {
+        return tempSabado;
+    }
+
+    public void setTempSabado(int tempSabado) {
+        this.tempSabado = tempSabado;
+    }
+
+    public int getTempDomingo() {
+        return tempDomingo;
+    }
+
+    public void setTempDomingo(int tempDomingo) {
+        this.tempDomingo = tempDomingo;
+    }
+
+    public static Object valoresAcimaMedia(int[] valores) {
+
+        for (int valor : valores) {
+            if (valor < -100 || valor > 100) {
+                return "Error";
+            }
+        }
+
+        double soma = 0;
+        for (int valor : valores) {
+            soma += valor;
+        }
+        double media = soma / valores.length;
+
+        int contadorAcimaMedia = 0;
+        for (int valor : valores) {
+            if (valor > media) {
+                contadorAcimaMedia++;
+            }
+        }
+        return contadorAcimaMedia;
+    }
+
+//    public static Object verifyError(int valor){
+//        if(valor == -1){
+//            return "Error";
+//        }
+//        return valor;
+//    }
+
+}
